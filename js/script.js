ac06e8bd52b324807f91e913eed1a2cdb64e3c40
RuBik <ruzz_08@mail.ru>
2022-12-01 13:48:53 +0300

    My first test commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+let lastFilm = prompt('Один из последних просмотренных фильмов?');
+let ratingFilm = prompt('На сколько оцените его?');
+
+
+let personalMovieDB = {
+    count :numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+personalMovieDB.movies[lastFilm] = ratingFilm;
+
+console.log(personalMovieDB);
